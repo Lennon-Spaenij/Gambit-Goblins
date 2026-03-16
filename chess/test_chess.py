7723e49a5c308018f126dd81a4dd957e4f388e3b
iLootYou <leon.kremer@student.nhlstenden.com>
2026-03-16 14:40:01 +0100

    Updated the code to use UCI format because it is easier to give to the robot (e2e4). And show the new FEN

--- chess/test_chess.py
+++ chess/test_chess.py
@@ -10,8 +10,12 @@ stockfish_path="C:\\Users\\leonk\\Downloads\\stockfish-windows-x86-64-avx2\\stoc
 board = chess.Board()
 engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
-print("Terminal chess VS Stockfish\n")
+# FEN
+current_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+new_fen = ""   
 
+print("Terminal chess VS Stockfish\n")
+print(f"Current FEN: {current_fen}")
 print("Please choose a color (black/white)")
 color_assignment = input("> ").strip().lower()
 
@@ -23,22 +27,25 @@ while not board.is_game_over():
             result = engine.play(board, chess.engine.Limit(time=2.0))
             board.push(result.move)
             print(f"Stockfish plays: {result.move}")
+            new_fen = board.fen()
         else:
-            print("Your move (e.g., 'e2e4' or 'Nf3'):")
+            print("Your move (e.g., 'e2e4' or 'g1f3'):")
             move_str = input("> ").strip()
             try: 
-                move = board.parse_san(move_str) # SAN like 'Nf3' Knight to f3
+                move = board.parse_uci(move_str) # uci like 'g1f3' Knight to f3
                 board.push(move)
+                new_fen = board.fen()
             except:
                 print("Invalid move")
                 continue
     else: # You chose white
         if board.turn == chess.WHITE:
-            print("Your move (e.g., 'e2e4' or 'Nf3'):")
+            print("Your move (e.g., 'e2e4' or 'g1f3'):")
             move_str = input("> ").strip()
             try: 
-                move = board.parse_san(move_str) # SAN like 'Nf3' Knight to f3
+                move = board.parse_uci(move_str) # uci like 'g1f3' Knight to f3
                 board.push(move)
+                new_fen = board.fen()
             except:
                 print("Invalid move")
                 continue
@@ -46,6 +53,11 @@ while not board.is_game_over():
             result = engine.play(board, chess.engine.Limit(time=2.0))
             board.push(result.move)
             print(f"Stockfish plays: {result.move}")
+            new_fen = board.fen()
+
+    if current_fen != new_fen:
+        current_fen = new_fen
+        print(f"Updated FEN: {current_fen}")
 
     print("\n" + str(board) + "\n")
     print(f"FEN: {board.fen()}\n")
